c8db598770a62b4815454815f6c020fb09d659c8
Kaushik <kaushik13k@Apples-MacBook-Air.local>
2024-09-28 14:44:18 +0530

    adde counter project

---
+++ tailwind-tut/src/app/page.tsx
@@ -0,0 +1,34 @@
+export default function Home() {
+  return (
+    // color
+    <div>
+      <div>
+        <h1 className="text-green-600">
+          Hello world!
+        </h1>
+        <h1 className="text-yellow-600">
+          Hello world-2!
+        </h1>
+        <h1 className="bg-green-600 text-white-600">
+          Hello world-2!
+        </h1>
+        <h1 className="border-8">
+          Hello world-2!
+        </h1>
+      </div>
+      {/* Typography */}
+      <div>
+        <h1 className="italic">Title 1</h1>
+        <h2 className="underline">Title 2</h2>
+        <h3 className="line-through">Title 3</h3>
+        <p className="text-xs note">A regullar paragraph</p>
+        <p className="text-sm">A description paragraph</p>
+        <p className="text-base">Little Note</p>
+      </div>
+      {/* Spaces and sizes */}
+      <div>
+        
+      </div>
+    </div>
+  )
+}
