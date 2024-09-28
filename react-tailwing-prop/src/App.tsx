656207d452f2a20cd96216db9055c3b4b0f3c083
Kaushik <kaushik13k@Apples-MacBook-Air.local>
2024-09-28 17:42:06 +0530

    added code for tailwing and props

---
+++ react-tailwing-prop/src/App.tsx
@@ -0,0 +1,16 @@
+import Card from "./cards/Card"
+
+function App() {
+
+  return (
+    <>
+    <h1 className="bg-green-500">Tailwind test</h1>
+    <br />
+    <Card name="hello"/>
+    <br />
+    <Card name="world"/>
+    </>
+  )
+}
+
+export default App
